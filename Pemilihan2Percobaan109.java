11cce6e7b548f52ea2fa80614f533d98e20c2198
ddickyy <dikiidarmawam05x2@gmail.com>
2024-10-02 10:26:07 +0700

    Sebelum revisi

---
+++ Pemilihan2Percobaan109.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * Pemilihan2Percobaan109
+ */
+public class Pemilihan2Percobaan109 {
+    public static void main(String[] args) {
+        
+    Scanner input09 = new Scanner (System.in);
+
+    System.out.print("Masukkan tahun: ");
+    int tahun = input09.nextInt();
+    
+    if ((tahun % 4)== 0) {
+        if ((tahun % 100)!= 0);
+            System.out.println("Tahun Kabisat");
+    } else 
+        System.out.println("Bukan tahun kabisat");
+    }
+
+    
+}
